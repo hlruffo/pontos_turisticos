c8c0fa0fa4c4b0376e730d246f3243729e0b0a2a
hlruffo <hlruffo@gmail.com>
2023-11-26 14:53:32 -0300

    feat: rotas de api para enderecos

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -19,11 +19,14 @@ from rest_framework import routers
 from core.api.viewsets import PontosTuristicosViewSet
 from atracoes.api.viewsets import AtracoesViewSet
 from avaliacoes.api.viewsets import AvaliacoesViewSet
+from comentarios.api.viewsets import ComentariosViewSet
 from enderecos.api.viewsets import EnderecosViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontosTuristicosViewSet)
 router.register(r'atracoes', AtracoesViewSet)
+router.register(r'avaliacoes', AvaliacoesViewSet)
+router.register(r'comentarios', ComentariosViewSet)
 router.register(r'enderecos', EnderecosViewSet)
 
 urlpatterns = [

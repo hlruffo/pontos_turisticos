16dfee1515777c26be10aca0b36a619908cf9118
hlruffo <hlruffo@gmail.com>
2023-11-26 14:32:47 -0300

    feat: rotas de api para atracoes

---
+++ atracoes/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from atracoes.models import Atracao
+
+class AtracoesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Atracao
+        fields= ('id','nome','descricao','horario_func', 'idade_minima')
+        #fields= '__all__'

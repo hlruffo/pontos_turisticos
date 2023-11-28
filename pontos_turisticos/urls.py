8c9e345712460ff3dc42ccde6f638e27158ef368
Hugo Ledertheil Ruffo <hlruffo@gmail.com>
2023-11-28 17:01:33 -0300

    feat: auth - em teste

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework import routers
 from core.api.viewsets import PontosTuristicosViewSet
 from atracoes.api.viewsets import AtracaoViewSet
@@ -38,4 +39,5 @@ router.register(r'enderecos', EnderecoViewSet)
 urlpatterns = [
     path('',include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-token-auth/', obtain_auth_token),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

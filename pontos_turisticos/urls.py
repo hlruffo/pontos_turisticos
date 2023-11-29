b352170a584ea3d8feb7126be2cee899fc55bcd2
Hugo Ledertheil Ruffo <hlruffo@gmail.com>
2023-11-29 07:53:00 -0300

    feat : implementação swagger

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -13,21 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework.authtoken.views import obtain_auth_token
+from django.contrib import admin
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import routers
-from core.api.viewsets import PontosTuristicosViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework import permissions
+
+
 from atracoes.api.viewsets import AtracaoViewSet
 from avaliacoes.api.viewsets import AvaliacaoViewSet
 from comentarios.api.viewsets import ComentarioViewSet
+from core.api.viewsets import PontosTuristicosViewSet
 from enderecos.api.viewsets import EnderecoViewSet
 
-
 router = routers.DefaultRouter()
-router.register(r'pontoturistico', PontosTuristicosViewSet)
+router.register(r'pontosturisticos', PontosTuristicosViewSet)
 #router.register(r'pontoturistico', PontosTuristicosViewSet, basename='PontosTuristicos') -> usado para
 #sobreescrever get_queryset
 router.register(r'atracoes', AtracaoViewSet)
@@ -35,9 +39,26 @@ router.register(r'avaliacoes', AvaliacaoViewSet)
 router.register(r'comentarios', ComentarioViewSet)
 router.register(r'enderecos', EnderecoViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Pontos Turísticos",
+        default_version='v1',
+        description="API do pontos turísticos com DRF",
+        contact=openapi.Contact(email="hugo.ruffo@brilliantmachine.com.br"),
+    ),
+    public=True,    
+)
+
+
+
 
 urlpatterns = [
     path('',include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-token-auth/', obtain_auth_token),
+    
+    #links para documentação
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

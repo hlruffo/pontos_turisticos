46005ca23fb63763dd34a0c39655fc877d0ad66d
sistemasinea <sistemasinea@gmail.com>
2023-12-12 12:34:45 -0300

    feat:  portugues e horario de brasilia

--- comentarios/admin.py
+++ comentarios/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Comentario  
-# Register your models here.
+from .actions import reprovar_comentarios, aprovar_comentarios
 
-admin.site.register(Comentario)
+# Register your models here.
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ['usuario', 'data', 'aprovado']
+    actions = [aprovar_comentarios, reprovar_comentarios]
+
+    
+    
+admin.site.register(Comentario, ComentarioAdmin)
+
